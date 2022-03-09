c4908338e37444236e42722d2cfd5e0c1de145d1
KimJiWoong02 <jiwoongkim02@gmail.com>
2022-03-09 09:50:45 +0900

    Login, Register HTML 수정 Login CSS 생성, Register CSS 수정 Login API 생성, 미들웨어 생성

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for, make_response
 
 app = Flask(__name__)
+# config 파일 참조
+app.config.from_pyfile('config.py')
 
 from pymongo import MongoClient
 import certifi
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.ugilq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+client = MongoClient(app.config['MONGODB'], tlsCAFile=certifi.where())
 db = client.travel
-
-# jwt 토큰을 만들 때 필요한 key
-SECRET_KEY = 'SPARTA'
 
 # jwt 패키지 사용
 import jwt
@@ -22,9 +21,102 @@ import hashlib
 
 import math
 
+# decorator jwt 검사를 위한 미들웨어로 활용
+from functools import wraps
+
+
+
+def home_decorator():
+    def _home_decorator(f):
+        @wraps(f)
+        def __home_decorator(*args, **kwargs):
+            accessToken_receive = request.cookies.get('accessToken')
+            refreshToken_receive = request.cookies.get('refreshToken')
+
+            # AccessToken이 유효한가? True or False
+            isValidAccessToken = getAccessToken()
+
+            # RefreshToken이 유효한가? True or False
+            isValidRefreshToken = getRefreshToken()
+
+            if isValidAccessToken and isValidRefreshToken:
+                # AccessToken과 RefreshToken 모두 유효 => 계속 진행
+                result = f()
+                return result
+
+            elif isValidAccessToken is not True and isValidRefreshToken:
+                # AccessToken 만료 RefreshToken 유효 => AccessToken 재발급
+                old_payload = jwt.decode(refreshToken_receive, app.config['SECRET_KEY'], algorithms=['HS256'])
+                payload = {
+                    'id': old_payload['id'],
+                    'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=app.config['ACCESSTOKENVALIDTIME'])
+                }
+                accessToken = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+
+                result = make_response(f())
+                result.set_cookie("accessToken", accessToken)
+                return result
+
+            elif isValidAccessToken and isValidRefreshToken is not True:
+                # AccessToken 유효 RefreshToken 만료 => RefreshToken 재발급
+                old_payload = jwt.decode(accessToken_receive, app.config['SECRET_KEY'], algorithms=['HS256'])
+                # refreshToken 생성 및 DB저장
+                payload = {
+                    'id': old_payload['id'],
+                    'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=app.config['REFRESHTOKENVALIDTIME'])
+                }
+                refreshToken = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+
+                db.users.update_one({'id': old_payload['id']}, {'$set': {'refreshToken': refreshToken}})
+
+                result = make_response(f())
+                result.set_cookie("refreshToken", refreshToken)
+                return result
+
+            else:
+                # AccessToken과 RefreshToken 모두 만료 => 로그인 페이지
+                return redirect(url_for("login"))
+
+        return __home_decorator
+
+    return _home_decorator
+
+
+def getAccessToken():
+    accessToken_receive = request.cookies.get('accessToken')
+    try:
+        jwt.decode(accessToken_receive, app.config['SECRET_KEY'], algorithms=['HS256'])
+        return True
+    except jwt.ExpiredSignatureError:  # 유효기간 만료된 토큰 디코드 시
+        return False
+    except jwt.exceptions.DecodeError:  # 해당 token이 올바르게 디코딩되지 않았다면
+        return False
+
+
+def getRefreshToken():
+    refreshToken_receive = request.cookies.get('refreshToken')
+    try:
+        payload = jwt.decode(refreshToken_receive, app.config['SECRET_KEY'], algorithms=['HS256'])
+
+        # 유저 ID로 유저 DB SELECT
+        user_info = db.users.find_one({"id": payload['id']})
+
+        # 쿠키에 저장된 refreshToken와 DB에 저장된 refreshToken가 같은지 비교
+        if refreshToken_receive == user_info['refreshToken']:
+            return True
+        else:
+            return False
+    except jwt.ExpiredSignatureError:  # 유효기간 만료된 토큰 디코드 시
+        return False
+    except jwt.exceptions.DecodeError:  # 해당 token이 올바르게 디코딩되지 않았다면
+        return False
+
+
 @app.route('/')
+@home_decorator()
 def home():
     return render_template('index.html')
+
 
 @app.route('/api/plans', methods=['GET'])
 def get_plans():
@@ -42,7 +134,7 @@ def get_plans():
     if len(query) > 0:
         option['location'] = query
 
-    plans = list(db.plan.find(option).skip((page-1)*limit).limit(limit).sort(sort, -1))
+    plans = list(db.plan.find(option).skip((page - 1) * limit).limit(limit).sort(sort, -1))
 
     results = []
     for document in plans:
@@ -60,13 +152,10 @@ def get_plans():
     return jsonify({'plans': results, 'last_page': last_page, 'page': page, 'none': none})
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-
 @app.route('/login')
 def login():
-    msg = request.args.get("msg")
-    return render_template('login.html', msg=msg)
+    # msg = request.args.get("msg")
+    return render_template('login.html')  # msg=msg)
 
 
 @app.route('/register')
@@ -79,7 +168,7 @@ def register():
 #################################
 
 # [회원가입 API]
-# id, pw, nickname을 받아서, mongoDB에 저장.
+# name, id, pw을 받아서, mongoDB에 저장.
 # 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
 @app.route('/api/sign_up/save', methods=['POST'])
 def api_register():
@@ -99,6 +188,8 @@ def api_register():
     return jsonify({'result': 'success', 'msg': '가입 성공!'})
 
 
+# [회원가입 아이디 중복체크 API]
+# id 받아서 DB에 값이 있는지 체크
 @app.route('/api/sign_up/check_dup', methods=['GET'])
 def api_check_dup():
     id_receive = request.args.get('id_give')
@@ -110,4 +201,47 @@ def api_check_dup():
     return jsonify({'result': 'success', 'msg': '멋진 아이디에요!'})
 
 
+# [로그인 API]
+# id, pw를 받아서 맞춰보고, 토큰을 만들어 발급.
+@app.route('/api/sign_in', methods=['POST'])
+def api_login():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
 
+    # 회원가입 때와 같은 방법으로 pw를 암호화.
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    # id, 암호화된pw을 가지고 해당 유저를 찾기.
+    result = db.users.find_one({'id': id_receive, 'pw': pw_hash})
+
+    # 찾으면 JWT 토큰을 만들어 발급합니다.
+    if result is not None:
+        # JWT 토큰에는, payload와 시크릿키가 필요하다.
+        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있다.
+        # 아래에선 id와 exp를 담았다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있다.
+        # exp에는 만료시간을 넣어준다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 난다.
+
+        # refreshToken 생성 및 DB저장
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=app.config['REFRESHTOKENVALIDTIME'])
+        }
+        refreshToken = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+        db.users.update_one({'id': id_receive}, {'$set': {'refreshToken': refreshToken}})
+
+        # accessToken 생성
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(app.config['ACCESSTOKENVALIDTIME'])
+        }
+        accessToken = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+
+        # token을 반환.
+        return jsonify({'result': 'success', 'accessToken': accessToken, 'refreshToken': refreshToken})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
