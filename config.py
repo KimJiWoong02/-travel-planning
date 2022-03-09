9e9ad22e56f39dc5ab6f115618ef8fbb13c4c34d
KimJiWoong02 <jiwoongkim02@gmail.com>
2022-03-09 09:45:06 +0900

    config 파일 생성

---
+++ config.py
@@ -0,0 +1,8 @@
+# jwt 토큰을 만들 때 필요한 key
+SECRET_KEY = 'TRAVELPLANNINGPROJECT'
+# 연결 할 mongodb 주소
+MONGODB = 'mongodb+srv://test:sparta@cluster0.ugilq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+# accessToken의 유효시간
+ACCESSTOKENVALIDTIME = 10
+# refreshToken의 유효시간
+REFRESHTOKENVALIDTIME = 20
