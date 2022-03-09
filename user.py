97afba23fe7397f5e25a965d9051075d6c8c1888
reload1bronze <nature9th@gmail.com>
2022-03-09 17:35:46 +0900

    프로필 페이지

--- user.py
+++ user.py
@@ -1,24 +1,25 @@
 import jwt
 from flask import Blueprint, render_template, redirect, url_for, request, jsonify
 from pymongo import MongoClient
+from werkzeug.utils import secure_filename
+import config
 
 SECRET_KEY = 'SPARTA'
 
-client = MongoClient(
-    'mongodb+srv://test:sparta@cluster0.ugilq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+client = MongoClient(config.MONGODB)
 db = client.travel
 
 bp = Blueprint('user', __name__, url_prefix='/user')
 
 
-@bp.route('/<username>')
-def user(username):
-    token_receive = request.cookies.get('mytoken')
+@bp.route('/<id>')
+def user(id):
+    token_receive = request.cookies.get(config.ACCESSTOKEN)
     try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        status = (username == payload["id"])
+        payload = jwt.decode(token_receive, config.SECRET_KEY, algorithms=['HS256'])
+        status = (id == payload["id"])
 
-        user_info = db.testuser.find_one({"username": username}, {"_id": False})
+        user_info = db.users.find_one({"id": id}, {"_id": False})
         return render_template('user.html', user_info=user_info, status=status)
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
@@ -26,20 +27,26 @@ def user(username):
 
 @bp.route('/edit', methods=['POST'])
 def edit_profile():
-    token_receive = request.cookies.get('mytoken')
+    token_receive = request.cookies.get(config.ACCESSTOKEN)
     try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        name_receive = request.form["username"]
-
-        # 파일 받아서 저장하는 방법
-        file = request.files["file_give"]
-        save_to = 'static/mypicture.jpg'
-        file.save(save_to)
-
+        payload = jwt.decode(token_receive, config.SECRET_KEY, algorithms=['HS256'])
+        id = payload['id']
+        name_receive = request.form['name_give']
         new_doc = {
-            "profile_name": name_receive
+            'name': name_receive
         }
-        db.testuser.update_one({'username': payload['id']}, {'$set': new_doc})
+        if 'file_give' in request.files:
+            file = request.files['file_give']
+
+            filename = secure_filename(file.filename)
+            extension = filename.split('.')[-1]
+            file_path = f'profile_pics/{id}.{extension}'
+
+            file.save('static/'+file_path)
+            new_doc['img'] = filename
+            new_doc['img_path'] = file_path
+
+        db.users.update_one({'id': payload['id']}, {'$set': new_doc})
         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
