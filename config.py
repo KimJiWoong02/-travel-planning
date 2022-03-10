0506545fc4331c82e35d2df9f7dfbd0bc5e50316
KimJiWoong02 <jiwoongkim02@gmail.com>
2022-03-10 17:48:45 +0900

    토큰 유효시간 릴리즈 버전으로 변경

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # jwt 토큰 secret_key
 SECRET_KEY = 'TRAVELPLANNINGPROJECT'
 # accessToken의 유효시간 (초)
-ACCESSTOKENVALIDTIME = 300
+ACCESSTOKENVALIDTIME = 10800
 # refreshToken의 유효시간 (초)
-REFRESHTOKENVALIDTIME = 400
+REFRESHTOKENVALIDTIME = 604800
 # accessToken의 이름
 ACCESSTOKEN = 'accessToken'
 # refreshToken의 이름
