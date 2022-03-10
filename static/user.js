4dccc4e69a43944d4e4c6cd8cd28f467b7663373
KimJiWoong02 <jiwoongkim02@gmail.com>
2022-03-10 17:45:34 +0900

    프로필 업데이트 또는 계획등록 시 작성유효한 토큰이 없으면 alert와 함께 로그인 화면으로 이동하는 기능 추가

--- static/user.js
+++ static/user.js
@@ -1,32 +1,35 @@
 $(document).ready(function () {
-  bsCustomFileInput.init();
+    bsCustomFileInput.init();
 });
 
 function sign_out() {
-  $.removeCookie("accessToken", { path: "/" });
-  $.removeCookie("refreshToken", { path: "/" });
-  window.location.href = "/login";
+    $.removeCookie("accessToken", {path: "/"});
+    $.removeCookie("refreshToken", {path: "/"});
+    window.location.href = "/login";
 }
 
 function update_profile() {
-  let name = $("#input-name").val();
-  let file = $("#input-pic")[0].files[0];
-  let form_data = new FormData();
-  form_data.append("file_give", file);
-  form_data.append("name_give", name);
+    let name = $("#input-name").val();
+    let file = $("#input-pic")[0].files[0];
+    let form_data = new FormData();
+    form_data.append("file_give", file);
+    form_data.append("name_give", name);
 
-  $.ajax({
-    type: "POST",
-    url: "/user/edit",
-    data: form_data,
-    cache: false,
-    contentType: false,
-    processData: false,
-    success: function (response) {
-      if (response["result"] == "success") {
-        alert(response["msg"]);
-        window.location.reload();
-      }
-    },
-  });
+    $.ajax({
+        type: "POST",
+        url: "/user/edit",
+        data: form_data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            if (response["result"] == "success") {
+                alert(response["msg"]);
+                window.location.reload();
+            } else {
+                alert('로그인이 만료되었습니다. 다시 로그인해 주십시오.');
+                window.location.reload();
+            }
+        },
+    });
 }
