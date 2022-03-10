5800e81ae1a4cecc11ebc8bbc6cd35c82e93ca20
llama-ste <andong1212@gmail.com>
2022-03-10 18:21:57 +0900

    파비콘, 로고 추가

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # jwt 토큰 secret_key
 SECRET_KEY = 'TRAVELPLANNINGPROJECT'
 # accessToken의 유효시간 (초)
-ACCESSTOKENVALIDTIME = 300
+ACCESSTOKENVALIDTIME = 10000
 # refreshToken의 유효시간 (초)
-REFRESHTOKENVALIDTIME = 400
+REFRESHTOKENVALIDTIME = 10000
 # accessToken의 이름
 ACCESSTOKEN = 'accessToken'
 # refreshToken의 이름
